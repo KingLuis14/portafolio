9a8f92030a96652d3314224da5a159e1c224c8b3
Luis Enrique <enriquecalero284@gmail.com>
2024-04-12 17:02:43 -0500

    =Cricital css incluido

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-  
-  devToolbar:{
+  site: "https://enrique-calero.netlify.app",
+  devToolbar: {
     enabled: false
   },
-  integrations: [icon(), react()]
+  integrations: [icon(), react(), sitemap(), playformInline()]
 });
