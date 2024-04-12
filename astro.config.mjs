3b4634d80e52c080d88cf94742fc7b81f847b355
Luis Enrique <enriquecalero284@gmail.com>
2024-04-12 16:22:24 -0500

    =Proyecto listo sin sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  
+  devToolbar:{
+    enabled: false
+  },
   integrations: [icon(), react()]
 });
